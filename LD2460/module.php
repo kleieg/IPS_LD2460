bfa01ff8d3a087c819b3e22a698ce741739f5cf7
kleieg <56699299+kleieg@users.noreply.github.com>
2026-02-15 19:00:46 +0100

    Refactor MyZwave_melder to HTMLVisuTestDuckCounters

--- LD2460/module.php
+++ LD2460/module.php
@@ -1,88 +1,163 @@
 <?php
 
-class MyZwave_melder extends IPSModule
-{
-    public function Create()
+    class HTMLVisuTestDuckCounters extends IPSModule
     {
-        //Never delete this line!
-        parent::Create();
+        
+        public function Create() {
+            //Never delete this line!
+            parent::Create();
 
-        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+            // Eigenschaften für die Darstellung eines Bildes
+            $this->RegisterPropertyInteger('Image', 1);
 
-        // properties
-        $this->RegisterPropertyString('Topic', '');
+            // Drei Eigenschaften für die dargestellten Zähler
+            $this->RegisterPropertyInteger('Counter1', 1);
+            $this->RegisterPropertyInteger('Counter2', 1);
+            $this->RegisterPropertyInteger('Counter3', 1);
 
-        // variables
-        $this->RegisterVariableString("Alarm_6", "Alarm_Serverraum", "Alarm");
-        $this->RegisterVariableInteger("Battery_6", "Battery_Serverraum", '~Battery.100');
-        $this->RegisterVariableString("Alarm_7", "Alarm_Waschkeller", "Alarm");
-        $this->RegisterVariableInteger("Battery_7", "Battery_Waschkeller", '~Battery.100');
-        $this->RegisterVariableString("Alarm_8", "Alarm_Treppe", "Alarm");
-        $this->RegisterVariableInteger("Battery_8", "Battery_Treppe", '~Battery.100');
-        $this->RegisterVariableString("Alarm_9", "Alarm_Schlafzimmer", "Alarm");
-        $this->RegisterVariableInteger("Battery_9", "Battery_Schlafzimmer", '~Battery.100');
+            // Eine Eigenschaft für die Hintergrundfarbe
+            $this->RegisterPropertyInteger('Color', 0xff0000);
 
+            // Visualisierungstyp auf 1 setzen, da wir HTML anbieten möchten
+            $this->SetVisualizationType(1);
+        }
+
+        public function ApplyChanges() {
+            parent::ApplyChanges();
+
+            // Aktualisiere registrierte Nachrichten
+            foreach ($this->GetMessageList() as $senderID => $messageIDs) {
+                foreach($messageIDs as $messageID) {
+                    $this->UnregisterMessage($senderID, $messageID);
+                }
+            }
+
+            foreach(['Counter1', 'Counter2', 'Counter3'] as $counterProperty) {
+                $this->RegisterMessage($this->ReadPropertyInteger($counterProperty), OM_CHANGENAME);
+                $this->RegisterMessage($this->ReadPropertyInteger($counterProperty), VM_UPDATE);
+            }
+
+            // Schicke eine komplette Update-Nachricht an die Darstellung, da sich ja Parameter geändert haben können
+            $this->UpdateVisualizationValue($this->GetFullUpdateMessage());
+        }
+
+        public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
+            // Man könnte noch auf weitere Nachrichten reagieren, um das ganze "vollständig" zu machen
+            // Werden registrierte Objekte gelöscht? Aktualisiert sich das Bild? Da dies aber nur ein Beispiel ist, lasse ich diese Nachrichten weg
+            foreach(['Counter1', 'Counter2', 'Counter3'] as $index => $counterProperty) {
+                if ($SenderID === $this->ReadPropertyInteger($counterProperty)) {
+                    switch ($Message) {
+                        case OM_CHANGENAME:
+                            // Teile der HTML-Darstellung den neuen Namen mit
+                            $this->UpdateVisualizationValue(json_encode([
+                                'name' . ($index + 1) => $Data[0]
+                            ]));
+                            break;
+
+                        case VM_UPDATE:
+                            // Teile der HTML-Darstellung den neuen Wert mit. Damit dieser korrekt formatiert ist, holen wir uns den von der Variablen via GetValueFormatted
+                            $this->UpdateVisualizationValue(json_encode([
+                                'value' . ($index + 1) => GetValueFormatted($this->ReadPropertyInteger($counterProperty))
+                            ]));
+                            break;
+                    }
+                }
+            }
+        }
+
+        public function RequestAction($Ident, $Value) {
+            // Nachrichten von der HTML-Darstellung schicken immer den Ident passend zur Eigenschaft und im Wert die Differenz, welche auf die Variable gerechnet werden soll
+            $variableID = $this->ReadPropertyInteger($Ident);
+            if (!IPS_VariableExists($variableID)) {
+                $this->SendDebug('Error in RequestAction', 'Variable to be updated does not exist', 0);
+                return;
+            }
+            SetValue($variableID, GetValue($variableID) + $Value);
+        }
+        
+        public function GetVisualizationTile() {
+            // Füge ein Skript hinzu, um beim laden, analog zu Änderungen bei Laufzeit, die Werte zu setzen
+            // Obwohl die Rückgabe von GetFullUpdateMessage ja schon JSON-codiert ist wird dennoch ein weiteres mal json_encode ausgeführt
+            // Damit wird dem String Anführungszeichen hinzugefügt und eventuelle Anführungszeichen innerhalb werden korrekt escaped
+            $initialHandling = '<script>handleMessage(' . json_encode($this->GetFullUpdateMessage()) . ');</script>';
+
+            // Füge statisches HTML aus Datei hinzu
+            $module = file_get_contents(__DIR__ . '/module.html');
+
+            // Gebe alles zurück. 
+            // Wichtig: $initialHandling nach hinten, da die Funktion handleMessage ja erst im HTML definiert wird
+            return $module . $initialHandling;
+        }
+
+        // Generiere eine Nachricht, die alle Elemente in der HTML-Darstellung aktualisiert
+        private function GetFullUpdateMessage() {
+            $counter1ID = $this->ReadPropertyInteger('Counter1');
+            $counter2ID = $this->ReadPropertyInteger('Counter2');
+            $counter3ID = $this->ReadPropertyInteger('Counter3');
+            $counter1Exists = IPS_VariableExists($counter1ID);
+            $counter2Exists = IPS_VariableExists($counter2ID);
+            $counter3Exists = IPS_VariableExists($counter3ID);
+            $result = [
+                'container' => $this->ReadPropertyInteger('Color'),
+                'counter1' => $counter1Exists,
+                'counter2' => $counter2Exists,
+                'counter3' => $counter3Exists,
+                'image' => ''
+            ];
+            if ($counter1Exists) {
+                $result['name1'] = IPS_GetName($counter1ID);
+                $result['value1'] = GetValueFormatted($counter1ID);
+            }
+            if ($counter2Exists) {
+                $result['name2'] = IPS_GetName($counter2ID);
+                $result['value2'] = GetValueFormatted($counter2ID);
+            }
+            if ($counter3Exists) {
+                $result['name3'] = IPS_GetName($counter3ID);
+                $result['value3'] = GetValueFormatted($counter3ID);
+            }
+            // Prüfe vorweg, ob ein Bild ausgewählt wurde
+            $imageID = $this->ReadPropertyInteger('Image');
+            if (IPS_MediaExists($imageID)) {
+                $image = IPS_GetMedia($imageID);
+                if ($image['MediaType'] === MEDIATYPE_IMAGE) {
+                    $imageFile = explode('.', $image['MediaFile']);
+                    $imageContent = '';
+                    // Falls ja, ermittle den Anfang der src basierend auf dem Dateitypen
+                    switch (end($imageFile)) {
+                        case 'bmp':
+                            $imageContent = 'data:image/bmp;base64,';
+                            break;
+    
+                        case 'jpg':
+                        case 'jpeg':
+                            $imageContent = 'data:image/jpeg;base64,';
+                            break;
+    
+                        case 'gif':
+                            $imageContent = 'data:image/gif;base64,';
+                            break;
+    
+                        case 'png':
+                            $imageContent = 'data:image/png;base64,';
+                            break;
+    
+                        case 'ico':
+                            $imageContent = 'data:image/x-icon;base64,';
+                            break;
+                    }
+
+                    // Nur fortfahren, falls Inhalt gesetzt wurde. Ansonsten ist das Bild kein unterstützter Dateityp
+                    if ($imageContent) {
+                        // Hänge base64-codierten Inhalt des Bildes an
+                        $imageContent .= IPS_GetMediaContent($imageID);
+                        $result['image'] = $imageContent;
+                    }
+                }
+            }
+            return json_encode($result);
+        }
+    
     }
 
-    public function ApplyChanges()
-    {
-        //Never delete this line!
-        parent::ApplyChanges();
-
-        $topic = $this->ReadPropertyString('Topic');
-        $this->SetReceiveDataFilter('.*' . $topic . '.*');
-    }
-
-    public function ReceiveData($JSONString)
-    {
-        $this->SendDebug('JSON', $JSONString, 0);
-        if (empty($this->ReadPropertyString('Topic'))) return;
-
-        $data = json_decode($JSONString);
-
-        $Buffer = $data;
-
-
-        //Melder #6 Serverraum
-        if (fnmatch('*ZWayVDev_zway_6-0-113-1-2-A', $Buffer->Topic)) {
-            $this->SendDebug('Alarm_6 Payload', $Buffer->Payload, 0);
-            $this->SetValue('Alarm_6', strval($Buffer->Payload));
-        }
-        if (fnmatch('*ZWayVDev_zway_6-0-128', $Buffer->Topic)) {
-            $this->SendDebug('Battery_6 Payload', $Buffer->Payload, 0);
-            $this->SetValue('Battery_6', intval($Buffer->Payload));
-        }
-
-        //Melder #7 Waschkeller
-        if (fnmatch('*ZWayVDev_zway_7-0-113-1-2-A', $Buffer->Topic)) {
-            $this->SendDebug('Alarm_7 Payload', $Buffer->Payload, 0);
-            $this->SetValue('Alarm_7', strval($Buffer->Payload));
-        }
-        if (fnmatch('*ZWayVDev_zway_7-0-128', $Buffer->Topic)) {
-            $this->SendDebug('Battery_7 Payload', $Buffer->Payload, 0);
-            $this->SetValue('Battery_7', intval($Buffer->Payload));
-        }
-
-        //Melder #8 Treppe
-        if (fnmatch('*ZWayVDev_zway_8-0-113-1-2-A', $Buffer->Topic)) {
-            $this->SendDebug('Alarm_8 Payload', $Buffer->Payload, 0);
-            $this->SetValue('Alarm_8', strval($Buffer->Payload));
-        }
-        if (fnmatch('*ZWayVDev_zway_8-0-128', $Buffer->Topic)) {
-            $this->SendDebug('Battery_8 Payload', $Buffer->Payload, 0);
-            $this->SetValue('Battery_8', intval($Buffer->Payload));
-        }
-
-        //Melder #8 Schalfzimmer
-        if (fnmatch('*ZWayVDev_zway_9-0-113-1-2-A', $Buffer->Topic)) {
-            $this->SendDebug('Alarm_9 Payload', $Buffer->Payload, 0);
-            $this->SetValue('Alarm_9', strval($Buffer->Payload));
-        }
-        if (fnmatch('*ZWayVDev_zway_9-0-128', $Buffer->Topic)) {
-            $this->SendDebug('Battery_9 Payload', $Buffer->Payload, 0);
-            $this->SetValue('Battery_9', intval($Buffer->Payload));
-        }
-
-    }
-
-}
+?>
